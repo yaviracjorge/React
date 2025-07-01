db0a60936b6aa336204812a075d263e8a6dc3d60
Andersoniza <ajc.iza@yavirac.edu.ec>
2025-07-01 12:36:40 -0500

    uso del use Effect -falta entender mas el ejemplo no es muy claro

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,69 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from './components'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const countMore = () => {
-    setCount((count) => count + 1)
+
+  const jsonData = async () => {
+   
+    try {
+      setLoading(true)
+      const request = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+      if (!request.ok) {
+        throw new Error("al obtener datos");
+      }
+      const response = await request.json();
+      setData(response);
+    } catch (err) {
+      setError(err as string);
+    } finally{
+      setLoading(false)
+    }
+  };
+
+  //para obtner datos hay que comunicase con un endpoit(un enpoint es un entidad externa al componente)
+  //se usa para
+  // comunicarnos con un endpoint
+  // operaciones asincronas
+  // se modifica parametros de entrada
+  useEffect(() => {
+    //siempre que tenga "use" es un hook, [] esto es un arreglo de dependencias
+    jsonData();
+  }, []);
+
+  if (loading) {
+    return <div>Cargando....</div>;
+  } else if (error) {
+    return <div>Hay un error {error}</div>;
   }
+
+  // useEffect(()=>{
+  //   console.log(loading)
+  // },[loading])
 
   return (
     <>
-      <Button label={`count is ${count}`} parentMethod={countMore}></Button>
+      <h1>Información del Pokémon</h1>
+      {data.name ? (
+        <div>
+          <h2>Nombre: {data.name}</h2>
+          <p>Altura: {data.height}</p>
+          <p>Peso: {data.weight}</p>
+          <h3>Habilidades:</h3>
+          <ul>
+            {data.abilities.map((item: any, i: any) => (
+              <li key={i}>{item.ability.name}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Cargando datos...</p>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,31 @@
-import "./Button.css"
+import "./Button.css";
 
-interface Props{
-    label: string,
-    parentMethod: ()=> void
-
+interface Props {
+  label: string;
+  parentMethod: () => void;
 }
 //se puede hacer de esta manera o usando la palabra funtion
-export const Button = ({label,parentMethod}:Props) => {
+export const Button = ({ label, parentMethod }: Props) => {
+  return (
+    <>
+      <button className="custom-button" onClick={parentMethod}>
+        {label}
+      </button>
+    </>
+  );
+};
 
-    return (
-        <>
-            <button className = "custom-button" onClick={parentMethod}>
-               {label}
-            </button>
-        </>
-    )
-}
+// creacion de un componente sin usar la funcion flecha
+// import "./Button.css";
+// interface Atributes {
+//   label: string;
+//   parenthMetodo: () => void;
+// }
+
+// export function Button({ label, parenthMetodo }: Atributes) {
+//   return (
+//     <button className="custom-button" onClick={parenthMetodo}>
+//       {label}
+//     </button>
+//   );
+// }
